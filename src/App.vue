6f589087117604f2bc63998d78fd172030d24700
Otoniel Andrade <otonielandrade7@gmail.com>
2021-01-07 11:25:58 -0600

    maquetacion finished

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
+:root {
+  --light-red: #c04c4b;
+  --dark-red: #a0403f;
+  --blue: #6ea4bb;
+  --light-yellow: #f0dba5;
+  --dark-yellow: #9b8754;
+  --dark-yellow-opacity: #b9a060;
+  --color-text: #e7e7e6;
+  --color-text-secondary: #f6f6f6;
+  --bg-color: #f2f2f2;
+  --black: #464646;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+html {
+  font-size: 62.5%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
+body {
+  font-size: 2rem;
+  background-color: var(--bg-color);
+  color: var(--black);
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+input {
+  outline: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top-section">
+      <div class="input-container">
+        <input
+          type="text"
+          placeholder="Ingresa el nombre del Pokemon..."
+          class="search"
+        />
+      </div>
+    </div>
+    <main class="main">
+      <div class="pokemon-list">
+        <section class="pokemon-container">
+          <div class="poke-basic-info">
+            <h3>Nombre</h3>
+            <div class="basic-power">
+              <span>power 1</span>
+              <span>power 2</span>
+            </div>
+          </div>
+          <div class="poke-image">image</div>
+        </section>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+<style>
+.top-section {
+  height: 15rem;
+  background-color: var(--light-red);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 0.4rem solid var(--black);
+}
+.input-container {
+  background-color: var(--bg-color);
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  width: 30%;
+  height: 5rem;
+  border-radius: 100px;
+  position: relative;
+  top: 7.5rem;
+  transition: box-shadow 0.2s;
+  border: 0.4rem solid var(--black);
+}
+.input-container:focus-within {
+  box-shadow: -2px 5px 8px rgba(0, 0, 0, 0.308);
+}
+.input-container .search {
+  background-color: var(--bg-color);
+  width: 90%;
+  border: none;
+  object-fit: fill;
+  font-size: 2.2rem;
+}
+
+.main {
+  background-color: var(--bg-color);
+  height: auto;
+  display: flex;
+  justify-content: center;
+}
+.pokemon-list {
+  margin: 10rem 0;
+  border: 1px red solid;
+  width: 75%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+}
+.pokemon-container {
+  color: var(--color-text);
+  height: 15rem;
+  background-color: var(--dark-yellow);
+  border-radius: 5px;
+  padding: 0 2rem;
+  display: flex;
+  align-items: center;
+  column-gap: 2rem;
+}
+.poke-basic-info {
+  margin: 1.5rem 0;
+}
+.basic-power span {
+  background-color: var(--dark-yellow-opacity);
+  font-size: 1.8rem;
+  display: block;
+  margin: 1rem 0;
+}
+</style>
